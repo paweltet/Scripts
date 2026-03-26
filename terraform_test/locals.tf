8b2d1545a0ab36a0fa50d15f1d2f04530a876088
paweltet <124254795+paweltet@users.noreply.github.com>
2026-03-26 11:07:01 +0100

    Update locals.tf

--- terraform_test/locals.tf
+++ terraform_test/locals.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
 resource "null_resource" "local_exec_test" {
   provisioner "local-exec" {
     interpreter = ["bash", "-c"]
